0ece37fc695361fab3414dba81213dd37c208d68
IgorTimoshin <igorek_timoshin@mail.ru>
2014-07-16 00:43:17 +0600

    задание 1.4

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace split_join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string d = Console.ReadLine();
+            string[] words = d.Split(' ');//расщепляет на слова
+            Console.WriteLine("В обратном порядке:");
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+
+
+                Console.Write(words[i] + " ");
+
+
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
